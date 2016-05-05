737dbeb72c23c82f3a4132da2884ad4dd5799d8a
Shanfan <chitchy@gmail.com>
2016-05-05 00:07:16 -0400

    set up gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('autoprefixer'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    port: 3000,
+    livereload: true
+  });
+});
+
+// Compile Stylesheets
+gulp.task('css', function () {
+  var processors = [ autoprefixer({browsers: ['last 1 version']}) ];
+    return gulp.src('./scss/all.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss(processors))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./css/'))
+        .pipe(connect.reload());
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('scss/*.scss', ['css']);
+});
+
+// Default Task
+gulp.task('default', ['css', 'watch', 'connect']);
