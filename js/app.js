54c232f87c7bdaa30248985087a43a48d79f4255
Shanfan <chitchy@gmail.com>
2016-05-27 23:31:25 -0400

    Added 'Update color interpolation' function to allow live update while typing. Added CSS styling

--- js/app.js
+++ js/app.js
@@ -21,6 +21,7 @@ function createSwatchInput(selector, id, color) {
   input.className = 'hex-input';
   input.type = 'text';
   input.value = color;
+  input.size = 7;
   input.style.color = lumaContrast(color);
 
   var wrapper = document.createElement('div');
@@ -39,6 +40,7 @@ function createSwatchInput(selector, id, color) {
       this.parentElement.style.background = chroma(this.value);
       this.parentElement.style.color = lumaContrast(this.value);
       this.parentElement.dataset.color = chroma(this.value);
+      updateInterpolation();
     }
   };
 }
@@ -46,26 +48,42 @@ function createSwatchInput(selector, id, color) {
 // 3:
 // The system returns 5 colors between start and end colors, using chroma.scale(['start', 'end']).colors(5)
 function interpolateSwatch(selector_1, selector_2, step) {
-  var start = document.querySelector(selector_1);
-  var end = document.querySelector(selector_2);
-  var interpolationList = chroma.scale([start.dataset.color, end.dataset.color]).colors(step);
+  var start = document.querySelector(selector_1),
+      end = document.querySelector(selector_2),
+      list = chroma.scale([start.dataset.color, end.dataset.color]).colors(step).slice(1, step-1);
 
-  for (var i = 1; i < step-1; i++){
-    var co = interpolationList[i];
-    var wrapper = document.createElement('div');
+  list.forEach(function(co){
+      var wrapper = document.createElement('div');
 
-    wrapper.className = 'swatch-interpolated';
-    wrapper.style.background = co;
-    wrapper.style.color = lumaContrast(co);
-    wrapper.dataset.color = co;
-    wrapper.innerHTML = `<span>${co}</span>`;
-    document.querySelector('#color-sheet').insertBefore(wrapper, end);
-  }
+      wrapper.className = 'swatch-interpolated';
+      wrapper.style.background = co;
+      wrapper.style.color = lumaContrast(co);
+      wrapper.dataset.color = co;
+      wrapper.innerHTML = `<span>${co.slice(1)}</span>`;
+
+      document.querySelector('#color-sheet').insertBefore(wrapper, end);
+
+  });
 }
 
+function updateInterpolation(){
+  var start = document.querySelector('#start').dataset.color,
+      end   = document.querySelector('#end').dataset.color,
+      list = chroma.scale([start, end]).colors(5),
+      divs = document.querySelectorAll('#color-sheet div');
+
+  list.forEach(function(co, i, list){
+      divs[i].style.background = co;
+      divs[i].style.color = lumaContrast(co);
+      divs[i].dataset.color = co;
+      if (i != 0 && i != list.length - 1){
+        divs[i].innerHTML = `<span>${co.slice(1)}</span>`;
+      }
+    });
+}
 
 function lumaContrast(c) {
-  var flip = chroma(c).luminance() > 0.45 ? 0.3 : 0.7;
+  var flip = chroma(c).luminance() > 0.45 ? 0 : 1;
   return chroma(c).luminance(flip);
 }
 
@@ -75,7 +93,7 @@ function lumaContrast(c) {
 
 
     // 4:
-    // I can specify an integer n (n>0), the system returns n colors between start and end.
+    // I can specify an integer n (n>3), the system returns n colors between start and end.
 
 // B: Color Interpolation With 3 Color Inputs and 2 Step Inputs
     // ...
