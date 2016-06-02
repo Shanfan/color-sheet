e8325bed22016565e805dfefb7a296f8ee0a8c37
Shanfan <chitchy@gmail.com>
2016-06-02 00:22:41 -0400

    d3 refactor: implified the matrix interpolation function

--- js/app.js
+++ js/app.js
@@ -1,43 +1,49 @@
-var initSheet = [{'id': 'upLeft', 'color': '575aFa', 'row': '0', 'column': '0'},
-                 {'id': 'lowLeft', 'color': 'bf75a7', 'row': '2', 'column': '0'},
-                 {'id': 'upRight', 'color': 'dddddd', 'row': '0', 'column': '2'},
-                 {'id': 'lowRight', 'color': '000000', 'row': '2', 'column': '2'}];
+var initData = interpolateMatrix('ff9afa', 'a175ff', '333456', '001888', 8, 9);
 
-var colorSheet = d3.select('#color-sheet').style({'height': 720});
+var colorSheet = d3.select('#color-sheet');
 
-colorSheet.selectAll('.swatch')
-          .data(initSheet).enter()
-          .append('div')
-          .attr({
-            'class': 'swatch',
-            'id': ƒ('id')
-          }).style({
-            'background': function(d) {return chroma(d.color)},
-            'color': function(d) {return lumaContrast(d.color)},
-            'left': function(d) {return `${d.column * 33.33333}%`},
-            'top': function(d) {return `${d.row * 720/3}px`},
-            'width': function(d) {return '33.33333%'},
-            'height': function(d) {return 720/3 + 'px'}
-          }).append('input')
-          .property({
-            'className': 'hex-input',
-            'type': 'text',
-            'value': ƒ('color'),
-            'size': 6
-          }).style({
-            'color': function(d) {return lumaContrast(d.color)}
-          }).on('keyup', function() {
-            var regex = /^[a-f\d]{6}\b|^[a-f\d]{3}\b/i,
-                checkInput = regex.exec(this.value) ? true : false;
+colorSheet.selectAll('tr')
+  .data(initData).enter()
+  .append('tr').selectAll('td')
+  .data(function(d) {return d}).enter()
+  .append('td.swatch')
+  .text(function(d){return d;})
+  .style({
+    'background': function(d) {return chroma(d)},
+    'color': function(d) {return lumaContrast(d)}
+  });
 
-            if (checkInput) {
-              this.style.color = lumaContrast(this.value);
-              this.parentElement.style.background = chroma(this.value);
-              this.parentElement.style.color = lumaContrast(this.value);
-            }
-          });
+//           d3.select('.color-input').on('keyup', function() {
+//             var regex = /^[a-f\d]{6}\b|^[a-f\d]{3}\b/i,
+//                 checkInput = regex.exec(this.value) ? true : false;
+//
+//             if (checkInput) {
+//               this.style.color = lumaContrast(this.value);
+//               this.parentElement.style.background = chroma(this.value);
+//               this.parentElement.style.color = lumaContrast(this.value);
+//             }
+//           });
 
 function lumaContrast(c) {
   var flip = chroma(c).luminance() > 0.45 ? 0 : 1;
   return chroma(c).luminance(flip);
+}
+
+function interpolateMatrix(color1, color2, color3, color4, rows, cols) {
+  list = [];
+  list1 = chroma.scale([color1, color2]).colors(cols);
+
+  list.push(list1);
+
+  list2 = chroma.scale([color1, color3]).colors(rows).slice(1, rows - 1);
+  list3 = chroma.scale([color2, color4]).colors(rows).slice(1, rows - 1);
+
+  list2.forEach(function(d, i){
+      list.push(chroma.scale([d,list3[i]]).colors(cols));
+  });
+
+  list4 = chroma.scale([color3, color4]).colors(cols);
+  list.push(list4);
+
+  return list;
 }
